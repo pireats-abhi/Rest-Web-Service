fccd1b3664ea2d126b8c10f3a092e1cf71821de0
pireats-abhi <pireats.abhi@gmail.com>
2020-04-29 20:00:05 +0600

    Initialized SimpleSpringBootRest demo project...

---
+++ SimpleSpringBootRest/src/main/java/org/ajgroup/boot/SimpleSpringBootRestApplication.java
@@ -0,0 +1,13 @@
+package org.ajgroup.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleSpringBootRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleSpringBootRestApplication.class, args);
+	}
+
+}
